8fb735a227dbc7033d4792607798655d38a01478
PirouFB <piroufunbeach@icloud.com>
2026-03-29 09:21:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ var orderPrices = [];
 var bouleMax = 0;
 var bouleCount = 0;
 
+/* ================= TOTAL ================= */
+
 function updateTotal(){
   document.getElementById("total").innerHTML = total.toFixed(2) + "€";
 }
@@ -19,6 +21,8 @@ function getCartTotal(){
   }
   return sum;
 }
+
+/* ================= CART ================= */
 
 function updateCart(){
 
@@ -35,6 +39,8 @@ function updateCart(){
 
   document.getElementById("cart").innerHTML = html;
 }
+
+/* ================= RESET ================= */
 
 function resetAll(){
 
@@ -58,6 +64,8 @@ function resetAll(){
   updateTotal();
   updateCart();
 }
+
+/* ================= ADD CART ================= */
 
 function addToCart(){
 
@@ -84,11 +92,15 @@ function addToCart(){
   updateCart();
 }
 
+/* ================= REMOVE ================= */
+
 function removeOrder(index){
   orders.splice(index,1);
   orderPrices.splice(index,1);
   updateCart();
 }
+
+/* ================= MAIN ================= */
 
 function selectMain(name, price, el){
 
@@ -106,4 +118,134 @@ function selectMain(name, price, el){
 
   updateTotal();
   updateCart();
+
+  if(name === "Glace") showGlaceStep1();
+  else showCrepePanini();
+}
+
+/* ================= CREPES ================= */
+
+function showCrepePanini(){
+
+  var html = "";
+  html += "<h3>Nappage</h3><div class='row'>" + build(nappage) + "</div>";
+  html += "<h3>Topping</h3><div class='row'>" + build(topping) + "</div>";
+  html += "<h3>Options</h3><div class='row'>" + build(options) + "</div>";
+
+  document.getElementById("dynamic").innerHTML = html;
+}
+
+/* ================= DATA ================= */
+
+var nappage = [
+["icon-nappage-nutella.png","Nutella"],
+["icon-nappage-sucre.png","Sucre"],
+["icon-nappage-chocolat.png","Chocolat"],
+["icon-nappage-creme-de-marron.png","Crème de Marrons"],
+["icon-nappage-fraise.png","Fraise"],
+["icon-nappage-caramel.png","Caramel"]
+];
+
+var topping = [
+["icon-topping-kinder-bueno.png","Kinder Bueno"],
+["icon-topping-oreo.png","Oreo"],
+["icon-topping-sprinkles.png","Sprinkles"],
+["icon-topping-chantilly.png","Chantilly"],
+["icon-topping-coco-rape.png","Coco Rapée"],
+["icon-topping-speculos.png","Speculos"]
+];
+
+var options = [
+["icon-options-boule-de-glace.png","Boule glace"],
+["icon-options-banane.png","Banane"],
+["icon-options-fraise.png","Fraise"],
+["icon-options-myrtille.png","Myrtille"],
+["icon-options-framboise.png","Framboise"],
+["icon-options-pistache-concassees.png","Pistache Concassées"]
+];
+
+/* ================= BUILD ================= */
+
+function build(list){
+
+  var html = "";
+
+  for(var i=0;i<list.length;i++){
+    html += "<div class='card' onclick=\"toggle(this,'" + list[i][1] + "')\">";
+    html += "<img src='" + list[i][0] + "'>";
+    html += "<p>" + list[i][1] + "</p></div>";
+  }
+
+  return html;
+}
+
+/* ================= TOGGLE ================= */
+
+function toggle(el,name){
+
+  if(el.classList.contains("selected")){
+    el.classList.remove("selected");
+
+    var newOrder = [];
+    for(var i=0;i<currentOrder.length;i++){
+      if(currentOrder[i] !== name){
+        newOrder.push(currentOrder[i]);
+      }
+    }
+    currentOrder = newOrder;
+
+    total -= 1;
+
+  } else {
+
+    el.classList.add("selected");
+    currentOrder.push(name);
+    total += 1;
+
+    if(name === "Boule glace"){
+      showExtraParfums();
+    }
+  }
+
+  updateTotal();
+  updateCart();
+}
+
+/* ================= GLACE ================= */
+
+function showGlaceStep1(){
+
+  var html = "";
+  html += "<div class='two'>";
+  html += "<div class='card' onclick=\"selectType(this,'Pot')\"><img src='icon-pot.png'><p>Pot</p></div>";
+  html += "<div class='card' onclick=\"selectType(this,'Cornet')\"><img src='icon-cornet.png'><p>Cornet</p></div>";
+  html += "</div>";
+
+  document.getElementById("dynamic").innerHTML = html;
+}
+
+function selectType(el,name){
+
+  var selected = document.querySelectorAll(".selected");
+  for(var i=0;i<selected.length;i++){
+    selected[i].classList.remove("selected");
+  }
+
+  el.classList.add("selected");
+  currentOrder.push(name);
+
+  updateCart();
+  showGlaceStep2();
+}
+
+function showGlaceStep2(){
+
+  var html = "";
+  html += "<div class='three'>";
+  html += "<div class='card' onclick=\"chooseBoules(this,1,2.5)\"><img src='icon-1-boule.png'><p>1 boule</p></div>";
+  html += "<div class='card' onclick=\"chooseBoules(this,2,4)\"><img src='icon-2-boules.png'><p>2 boules</p></div>";
+  html += "<div class='card' onclick=\"chooseBoules(this,3,5)\"><img src='icon-3-boules.png'><p>3 boules</p></div>";
+  html += "</div>";
+
+  document.getElementById("dynamic").innerHTML = html;
 }
