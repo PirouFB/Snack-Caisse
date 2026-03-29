f4b47cb784a87a05770967330902e43d337159ad
PirouFB <piroufunbeach@icloud.com>
2026-03-29 17:26:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,57 @@
-const CACHE_NAME = "caisse-cache-v1";
+const CACHE_NAME = "caisse-cache-v2";
 
 const urlsToCache = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./icon.png"
+  "./icon.png",
+
+  // MAIN
+  "./icon-crepe.png",
+  "./icon-panini.png",
+  "./icon-glace.png",
+
+  // GLACE
+  "./icon-pot.png",
+  "./icon-cornet.png",
+  "./icon-1-boule.png",
+  "./icon-2-boules.png",
+  "./icon-3-boules.png",
+
+  // NAPPAGE
+  "./icon-nappage-nutella.png",
+  "./icon-nappage-sucre.png",
+  "./icon-nappage-chocolat.png",
+  "./icon-nappage-creme-de-marron.png",
+  "./icon-nappage-fraise.png",
+  "./icon-nappage-caramel.png",
+
+  // TOPPING
+  "./icon-topping-kinder-bueno.png",
+  "./icon-topping-oreo.png",
+  "./icon-topping-sprinkles.png",
+  "./icon-topping-chantilly.png",
+  "./icon-topping-coco-rape.png",
+  "./icon-topping-speculos.png",
+
+  // OPTIONS
+  "./icon-options-boule-de-glace.png",
+  "./icon-options-banane.png",
+  "./icon-options-fraise.png",
+  "./icon-options-myrtille.png",
+  "./icon-options-framboise.png",
+  "./icon-options-pistache-concassees.png",
+
+  // PARFUMS
+  "./icon-parfum-glace-chocolat.png",
+  "./icon-parfum-glace-fraise.png",
+  "./icon-parfum-glace-vanille.png",
+  "./icon-parfum-glace-menthe.png",
+  "./icon-parfum-glace-caramel.png",
+  "./icon-parfum-glace-noix-de-coco.png",
+
+  // CHANTILLY
+  "./icon-chantilly-oui.png",
+  "./icon-chantilly-non.png"
 ];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
