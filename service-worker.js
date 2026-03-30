a96e7d14db4f4bec6c899b02422c4bf2f2721030
PirouFB <piroufunbeach@icloud.com>
2026-03-30 17:29:26 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,21 @@
-const CACHE_NAME = "caisse-cache-v6";
+const CACHE_NAME = "caisse-cache-v10";
 
 const urlsToCache = [
   "./",
   "./index.html",
   "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./icon.png",
-
-  "./logo-vague.png",
-  "./logo-manga.png",
-
-  "./icon-crepe.png",
-  "./icon-panini.png",
-  "./icon-glace.png",
-
-  "./icon-pot.png",
-  "./icon-cornet.png",
-  "./icon-1-boule.png",
-  "./icon-2-boules.png",
-
-  "./icon-chantilly-oui.png",
-  "./icon-chantilly-non.png"
+  "./script.js"
 ];
 
 // INSTALL
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// FETCH
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
-
-// UPDATE CACHE
+// ACTIVATE
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -47,5 +24,13 @@ self.addEventListener("activate", e => {
             .map(k => caches.delete(k))
       )
     )
+  );
+  self.clients.claim();
+});
+
+// FETCH (🔥 NETWORK FIRST)
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
